98da08dd518ad999ea2ecd12bd2b89321c0eae56
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-09-04 23:31:00 +0200

    wip

--- app/Livewire/WelcomeComponent.php
+++ app/Livewire/WelcomeComponent.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class WelcomeComponent extends Component
 {
+    #[Title('Home - Garden of Eden Produce')]
     public function render()
     {
         return view('livewire.welcome-component');
