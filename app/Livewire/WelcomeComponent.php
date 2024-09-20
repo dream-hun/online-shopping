618505dd8c75b4d14c23b6b28abbfe7771084eeb
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-09-20 16:36:33 +0200

    wip

--- app/Livewire/WelcomeComponent.php
+++ app/Livewire/WelcomeComponent.php
@@ -10,6 +10,30 @@ class WelcomeComponent extends Component
     #[Title('Home - Garden of Eden Produce')]
     public function render()
     {
+        seo()
+            ->locale('en_GB')
+            ->title('Home')
+            ->description('Garden of Eden Produce is a Rwandan company that organically grows and delivers a variety of fresh groceries, including many previously unavailable on the Rwandan market')
+            ->keywords(
+                'online shopping in rwanda',
+                'online shopping sites in rwanda',
+                'kigali',
+                'grocery store kigali',
+                'organic produce rwanda',
+                'fresh groceries kigali',
+                'Garden of Eden Produce',
+                'rwandan organic company'
+            )
+            ->url(url()->current())
+            ->canonical(url()->current())
+            ->canonicalEnabled(true)
+            ->images(asset('images/garden-of-eden-produce.webp'))
+            ->twitterCard('summary_large_image')
+            ->twitterSite('@GardenofEdenPr')
+            ->twitterCreator('@GardenofEdenPr')
+            ->robots('index', 'follow');
+
+
         return view('livewire.welcome-component');
     }
 }
