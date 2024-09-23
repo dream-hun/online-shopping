91002e0b8149c88961a5a04697835d702dae5b0c
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-09-24 00:59:35 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Category;
 use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         View::share('setting', Setting::first());
         View::share('categories', Category::withCount('products')->latest()->get(['id', 'name', 'slug']));
     }
