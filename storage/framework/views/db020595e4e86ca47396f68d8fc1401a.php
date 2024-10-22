1783b80ae339c58a57544271d88ce71a24c35da8
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-10-22 17:50:51 +0200

    wip

--- storage/framework/views/db020595e4e86ca47396f68d8fc1401a.php
+++
@@ -1,5 +0,0 @@
-<!-- Metadata generated using Honeystone SEO: https://github.com/honeystone/laravel-seo -->
-    <?php echo $generated; ?>
-
-<!-- End Honeystone SEO -->
-<?php /**PATH /home/hunter/Codes/online-shopping/vendor/honeystone/laravel-seo/src/../resources/views/layout.blade.php ENDPATH**/ ?>
