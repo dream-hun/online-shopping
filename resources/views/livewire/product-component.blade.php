74527a0e84a36b8ffa2418efdae9bfcb78829671
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2024-11-14 22:58:34 +0200

    wip

--- resources/views/livewire/product-component.blade.php
+++ resources/views/livewire/product-component.blade.php
@@ -65,15 +65,5 @@
             </div>
         </div>
     </section>
-    <div class="md:flex md:items-center md:justify-between py-5">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Related Products</h2>
 
-    </div>
-    <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-3 mt-5">
-        @forelse($relatedProducts as $item)
-            <livewire:front-product-component :product="$item"/>
-        @empty
-            <p class="text-lg"> No product found</p>
-        @endforelse
-    </div>
 </div>
