9197e976c6396f46728c331a33774f70d14b9abc
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2024-11-18 13:09:22 +0200

    wip

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -62,11 +62,7 @@
                     <div class="flex items-center space-x-4">
                         <livewire:search-component/>
                         <livewire:cart-counter-component/>
-                        <button @click="mobileMenuOpen = !mobileMenuOpen" class="p-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                            </svg>
-                        </button>
+
                     </div>
                 </div>
 
