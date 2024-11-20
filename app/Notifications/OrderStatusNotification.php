18abf9e3098b2c588030f268bb5906eaa6ff7682
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2024-11-20 13:00:32 +0200

    wip

--- app/Notifications/OrderStatusNotification.php
+++ app/Notifications/OrderStatusNotification.php
@@ -10,48 +10,22 @@ class OrderStatusNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct(private $status)
+    private $status;
+
+    public function __construct($status)
     {
-        //
+        $this->status = $status;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject('Your order status has been updated')
-            ->greeting('Hello!')
-            ->line('Your order status has been '.$this->status->value)
-            ->line('Thank you for shopping with us!')
-            ->salutation('Best Regards')
-            ->salutation('The '.config('app.name').' Team');
-
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
+            ->line('Your order status was changed to '.$this->status)
+            ->line('Thank you for shopping with us!');
     }
 }
